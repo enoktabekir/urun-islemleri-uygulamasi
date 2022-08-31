7cdcfd6c310d9cc2bc67f0241cc42d9628570a19
enoktabekir <e.bekr99@gmail.com>
2022-08-31 14:34:23 +0300

    urun-islemleri-uygulamasi
    vuejs ile urun giris cikis uygulamasi

---
+++ urun-islemleri-uygulaması/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <app-header></app-header>
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import Header from "./components/shared/Header.vue"
+import Footer from "./components/shared/Footer.vue"
+export default {
+  components: {
+    appHeader: Header,
+    appFooter: Footer,
+  },
+  created() {
+    this.$store.dispatch("getTradeResult");
+    this.$store.dispatch("initApp");
+  }
+}
+</script>
+
+<style>
+.fade-enter {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: opacity .2s ease-out;
+}
+
+.fade-leave {}
+
+.fade-leave-active {
+  transition: opacity .2s ease-out;
+  opacity: 0;
+}
+</style>
